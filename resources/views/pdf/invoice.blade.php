c31bc764680100c27522cd1d0a08c776dbe2bf75
Ameer Hamza <ameer.hamza@aft.com.pk>
2024-08-07 22:37:35 +0500

    Fixes

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -170,7 +170,7 @@
             </tr>
             <tr>
                 <td>
-                    <!-- <img height="auto" width="75" src="{{ $order->brand->image }}" alt=""> -->
+                    <img height="auto" width="75" src="{{ $order->brand->image }}" alt="">
                 </td>
                 <td>{{ strtoupper($order->brand->name) }}</td>
                 <td>{{ ucwords($order->product->name) }}</td>
